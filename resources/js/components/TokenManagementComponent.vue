7913ff4212ed6e4199561a48feddaa1e6d427e9e
EvgeniyKozenok <kozenokevgeniy@gmail.com>
2020-11-18 09:42:48 +0200

    passport token

---
+++ resources/js/components/TokenManagementComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Token Management</div>
+
+                    <div class="card-body">
+                        <clients/>
+                        <authorized-clients/>
+                        <personal-access-tokens/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import Clients from './passport/Clients';
+    import AuthorizedClients from './passport/AuthorizedClients';
+    import PersonalAccessTokens from './passport/PersonalAccessTokens';
+
+    export default {
+        components: {Clients, AuthorizedClients, PersonalAccessTokens},
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/home');
+    return redirect('home');
 });
 
 Auth::routes(['reset' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
